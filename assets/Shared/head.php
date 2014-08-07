d8ba55362694a60224007e80345eceb7a4e4bc12
Martin <martinlcalvert@gmail.com>
2014-08-06 19:07:17 -0700

    Updated Awana/Bible/Contact pages

--- assets/Shared/head.php
+++ assets/Shared/head.php
@@ -12,5 +12,5 @@
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../../assets/ico/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="../../assets/ico/apple-touch-icon-57-precomposed.png">
 <link rel="shortcut icon" href="assets/ico/favicon.ico" type="image/x-icon">
-<title>Palm Canyon Community Church</title>
+<title>Palm Canyon Community Church | </title>
 </head>
--- assets/Shared/navbar.php
+++ assets/Shared/navbar.php
@@ -14,9 +14,9 @@
             </button>
             
             
-            <a class="navbar-brand visible-sm-block" href="index.html">PC3</a>
-            <a class="navbar-brand hidden-sm hidden-md" href="index.html">Palm Canyon Community Church</a>
-            <a class="navbar-brand visible-md-block" href="index.html">Palm Canyon</a>
+            <a class="navbar-brand visible-sm-block" href="index.php">PC3</a>
+            <a class="navbar-brand hidden-sm hidden-md" href="index.php">Palm Canyon Community Church</a>
+            <a class="navbar-brand visible-md-block" href="index.php">Palm Canyon</a>
           </div>
             <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
             <div class="navbar-collapse collapse">
@@ -25,67 +25,67 @@
                  <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">I'm New <i class="fa fa-angle-double-down"></i></a>
                   <ul class="dropdown-menu">
-                    <li><a href="services.html">When are the services?</a></li>
-                    <li><a href="services.html#length">How long do services last?</a></li>
-                    <li><a href="services.html#length">What do I wear?</a></li>
-                    <li><a href="services.html#length">What's the music like?</a></li>
-                    <li><a href="services.html#kids">What do you have for kids?</a></li>
-                    <li><a href="services.html#kids">What do you have for teens?</a></li>
-                    <li><a href="about.html">What do you believe?</a></li>
-                    <li><a href="about.html#vision">What's your vision?</a></li>
-                    <li><a href="about.html#vision">What are your core values?</a></li>
-                    <li><a href="about.html#bill">Who is Lead Pastor Bill Martin?</a></li>
-                    <li><a href="staff.html">Who's the staff leadership team?</a></li>
-                    <li><a href="contact.html">Can you give me directions?</a></li>
+                    <li><a href="services.php">When are the services?</a></li>
+                    <li><a href="services.php#length">How long do services last?</a></li>
+                    <li><a href="services.php#length">What do I wear?</a></li>
+                    <li><a href="services.php#length">What's the music like?</a></li>
+                    <li><a href="services.php#kids">What do you have for kids?</a></li>
+                    <li><a href="services.php#kids">What do you have for teens?</a></li>
+                    <li><a href="about.php">What do you believe?</a></li>
+                    <li><a href="about.php#vision">What's your vision?</a></li>
+                    <li><a href="about.php#vision">What are your core values?</a></li>
+                    <li><a href="about.php#bill">Who is Lead Pastor Bill Martin?</a></li>
+                    <li><a href="staff.php">Who's the staff leadership team?</a></li>
+                    <li><a href="contact.php">Can you give me directions?</a></li>
                   </ul>
                 </li>
                  <li class="dropdown">
                   <a href="#" class="dropdown-toggle hidden-sm" data-toggle="dropdown">Adult Ministries<i class="fa fa-angle-double-down"></i></a>
                   <a href="#" class="dropdown-toggle visible-sm-block" data-toggle="dropdown">Adult's<i class="fa fa-angle-double-down"></i></a>
                   <ul class="dropdown-menu">
-                    <li><a href="adtov.html">Overview</a></li>
-                    <li><a href="adtov.html#pst">Personal Spiritual Trainer</a></li>
-                    <li><a href="adtov.html#care">Care Team</a></li>
-                    <li><a href="adtov.html#hosp">Hospitality team</a></li>
-                    <li><a href="adtov.html#int">Intercessory Prayer</a></li>
-                    <li><a href="amp.html">Amplified Worship</a></li>
-                    <li><a href="sermons.html">Sermons</a></li>
-                    <li><a href="vol.html">Volunteering</a></li>
+                    <li><a href="adtov.php">Overview</a></li>
+                    <li><a href="adtov.php#pst">Personal Spiritual Trainer</a></li>
+                    <li><a href="adtov.php#care">Care Team</a></li>
+                    <li><a href="adtov.php#hosp">Hospitality team</a></li>
+                    <li><a href="adtov.php#int">Intercessory Prayer</a></li>
+                    <li><a href="amp.php">Amplified Worship</a></li>
+                    <li><a href="sermons.php">Sermons</a></li>
+                    <li><a href="vol.php">Volunteering</a></li>
                     
-                 <li><a href="giving.html">Giving</a></li>
+                 <li><a href="giving.php">Giving</a></li>
                   </ul>
                 </li>
                  <li class="dropdown">
                   <a href="#" class="dropdown-toggle hidden-sm" data-toggle="dropdown">Student Ministries<i class="fa fa-angle-double-down"></i></a>
                   <a href="#" class="dropdown-toggle visible-sm-block" data-toggle="dropdown">Student's<i class="fa fa-angle-double-down"></i></a>
                   <ul class="dropdown-menu">
-                    <li><a href="stuov.html">Overview</a></li>
-                    <li><a href="stuov.html#adult">Just For Parents</a></li>
+                    <li><a href="stuov.php">Overview</a></li>
+                    <li><a href="stuov.php#adult">Just For Parents</a></li>
                   </ul>
                 </li>
                  <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">PC Kids<i class="fa fa-angle-double-down"></i></a>
                   <ul class="dropdown-menu">
-                    <li><a href="kidov.html">What To Expect</a></li>
-                    <li><a href="kidsun.html">Sundays</a></li>
-                    <li><a href="kidvid.html">Thursdays</a></li>
-                    <li><a href="kidhelp.html">Kids Helping Kids</a></li>
-                    <li><a href="vbs.html">VBS</a></li>
-                    <li><a href="fall.html">Fall Festival</a></li>
-                    <li><a href="bible.html">Reading Your Bible Is Fun</a></li>
-                    
-                    <li><a href="follow.html">How To Follow Christ</a></li>
-                    <li><a href="kidov.html">What To Expect</a></li>
+                    <li><a href="kidov.php">What To Expect</a></li>
+                    <li><a href="kidsun.php">Sundays</a></li>
+                    <li><a href="kidvid.php">Thursdays</a></li>
+                    <li><a href="kidhelp.php">Kids Helping Kids</a></li>
+                    <li><a href="vbs.php">VBS</a></li>
+                    <li><a href="fall.php">Fall Festival</a></li>
+                    <li><a href="bible.php">Reading Your Bible Is Fun</a></li>
+                    <li><a href="awana.php">Awana</a></li>
+                    <li><a href="follow.php">How To Follow Christ</a></li>
+                    <li><a href="kidov.php">What To Expect</a></li>
                   </ul>
                 </li><li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Life Groups<i class="fa fa-angle-double-down"></i></a>
                   <ul class="dropdown-menu">
-                    <li><a href="groups.html">Schedule</a></li>
-                    <li><a href="groups.html#leaders">Leaders & Group Info</a></li>
+                    <li><a href="groups.php">Schedule</a></li>
+                    <li><a href="groups.php#leaders">Leaders & Group Info</a></li>
                   </ul>
                 </li>
-                 <li><a href="events.html">Events</a></li>
-                 <li><a href="missions.html">Missions</a></li>
+                 <li><a href="events.php">Events</a></li>
+                 <li><a href="missions.php">Missions</a></li>
               </ul>
             </div><!--/.nav-collapse -->
           </div><!-- /.navbar-inner -->
