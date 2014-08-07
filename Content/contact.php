fbfa651122fd8681018e486cb5435f8d3714212b
Martin <martinlcalvert@gmail.com>
2014-08-06 19:13:55 -0700

    Updated for responsive iframes

--- Content/contact.php
+++ Content/contact.php
@@ -3,7 +3,7 @@
 <body class="wood">
 <? include 'navbar.php' ?>
     <div class="container marketing space color">
-    <div class="col-xs-12"><h1 class="text-center">Contact & Directions</h1></div>
+    <div class="col-xs-12"><h1 class="text-center">Contact & Directions</h1>g</div>
         <div class="row">
         <div class="col-xs-12 col-sm-6 lead">Bill Martin - Lead Pastor<br><a href="mailto:bill@palmcanyon.org">bill@palmcanyon.org</a>
         <br>Tim Shafer - Student Ministries Pastor<br> <a href="mailto:tim@palmcanyon.org">tim@palmcanyon.org</a>
@@ -13,7 +13,7 @@
         <br>Leslie Hitchcock - Business Administrator <br><a href="mailto:leslie@palmcanyon.org">leslie@palmcanyon.org</a>
         <br>Martin Calvert - Webmaster <br><a href="mailto:martinlcalvert@gmail.com">martinlcalvert@gmail.com</a><br>
         </div>
-        <div class="col-xs-12 col-sm-6">
+        <div class="col-xs-12 col-sm-6 Flexible-container">
         <h4>25550 Ironwood Ave Moreno Valley, CA 92557<br>Phone: 951.247.2273 — Fax: 951.243.1197</h4>
         <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=201274435064039598516.0004e25e36597c1e72dbc&amp;ie=UTF8&amp;ll=33.947499,-117.216589&amp;spn=0,0&amp;t=m&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=201274435064039598516.0004e25e36597c1e72dbc&amp;ie=UTF8&amp;ll=33.947499,-117.216589&amp;spn=0,0&amp;t=m&amp;source=embed" style="color:#0000FF;text-align:left">Palm Canyon Community Church</a> in a larger map</small>
         </div>
