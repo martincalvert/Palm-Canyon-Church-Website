d6184e09515dae9a9cb2969e4744f1ed221081b6
Martin <martinlcalvert@gmail.com>
2014-08-08 11:35:02 -0700

    Finsihed follow page

--- Content/follow.php
+++ Content/follow.php
@@ -4,31 +4,43 @@
 <body class="wood">
  <? include 'navbar.php' ?>
     <div class="container marketing space">
-    <div class="col-xs-12 text-center"><h1>Awana</h1></div>
+    <div class="col-xs-12 text-center"><h1>How To Follow Christ</h1></div>
       <!-- Three columns of text below the carousel -->
       <div class="row">
-        <div class="col-sm-6 col-xs-12">
+       <div class="col-sm-6 col-xs-12">
         <h3>Following Christ</h3>
         <a class="btn btn-large btn-info" href="#followModal" role="button" data-toggle="modal">Video <i class="fa fa-play"></i></a>
-        </div>
-        <div class="col-sm-6 col-xs-12">
+       </div>
+       <div class="col-sm-6 col-xs-12">
         <h3>Baptism</h3>
         <a class="btn btn-large btn-info" href="#baptismModal" role="button" data-toggle="modal">Video <i class="fa fa-play"></i></a>
+       </div>
       </div><!-- /.row -->
       <!-- START THE FEATURETTES -->
       <hr class="featurette-divider">
        <? include 'features.php' ?>
       <hr class="featurette-divider">
-      <div id="followModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  		<div class="modal-body text-center">
-		 <iframe width="1280" height="720" src="http://www.youtube.com/embed/jEprqbmDpuI" frameborder="0" allowfullscreen></iframe>
-  		</div>
-	</div>
-    <div id="baptismModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  		<div class="modal-body text-center">
-        <iframe width="640" height="360" src="http://youtu.be/Q51yi_HxNkY" frameborder="0" allowfullscreen></iframe>
-  		</div>
-	</div>
+
+      <div class="modal" id="followModal">
+       <div class="modal-dialog modal-lg">
+	 <div class="modal-content">
+	   <div class="modal-body text-center">
+	     <iframe width="853" height="480" src="//www.youtube.com/embed/Q51yi_HxNkY" frameborder="0" allowfullscreen></iframe>
+	   </div>
+	 </div><!-- /.modal-content -->
+       </div><!-- /.modal-dialog -->
+     </div><!-- /.modal -->
+      <div class="modal" id="baptismModal" tabindex="-1" role="dialog" aria-labelledby="videoModal" aria-hidden="true">
+       <div class="modal-dialog modal-lg">
+	 <div class="modal-content">
+	   <div class="modal-body text-center" id="videoModal">
+	     <iframe width="853" height="480" src="//www.youtube.com/embed/Q51yi_HxNkY" frameborder="0" allowfullscreen></iframe>
+	   </div>
+     
+	 </div><!-- /.modal-content -->
+       </div><!-- /.modal-dialog -->
+     </div><!-- /.modal -->
+
     <? include 'footer.php' ?>
 <script>
  document.title+="Following Christ"
