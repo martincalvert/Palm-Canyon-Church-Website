a55f32bb9e9cb6046325a9d0ee4e19d057da4bcf
Martin <martinlcalvert@gmail.com>
2014-08-08 18:58:39 -0700

    Updated groups & giving pages

---
+++ Content/goodsub.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<?php include 'head.php' ?>
+<body class="wood">
+
+<? include 'navbar.php' ?>
+
+    <div class="container marketing space color">
+      <div class="row">
+        <div class="col-xs-12 text-center">
+        <h2>Submission Was Successful!</h2>
+        <h5>Thank you! This page will redirect you home</h5>
+        </div>
+      </div><!-- /.row -->
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <? include 'features.php' ?>
+
+      <hr class="featurette-divider">
+
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <? include 'footer.php' ?>
+</body>
+
+</html>
