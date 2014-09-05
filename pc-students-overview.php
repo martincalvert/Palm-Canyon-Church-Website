7ba7e66c1cce5e7a0c777f1b56d17de844c9bc92
Martin <martinlcalvert@gmail.com>
2014-09-04 18:28:19 -0700

    Updated student ministries pages

--- pc-students-overview.php
+++ pc-students-overview.php
@@ -10,30 +10,23 @@
       <div class="col-xs-12">
         <p>Something is happening in Moreno Valley that could change your life and impact our world forever... It's right here at Palm Canyon a great place for middle school and high school students. Once you've experienced the Student Ministries, get involved in the programs, meet other students, and get to know the leaders, you'll say like many others have said before you...<strong>"I LOVE THIS PLACE."</strong></p></div>
         <div class="col-xs-12 text-center"><h2 id="dv8">MIDDLE SCHOOL</h2>
-        <div class="col-xs-12 col-sm-4"><p><h4>DV8 (6th - 8th Grade)</h4>
+        <div class="col-xs-12 col-sm-6"><p><h4>DV8 (6th - 8th Grade)</h4>
         DV8 (Deviate) is our mid-week program designed to reach out to all middle school students, especially those who do not go to church. Teams compete in a lot of fun, high-energy games before learning about God during our "God Time." Our optional small group time is a great way for students who are serious about God to get more involved by connection with a leader who cares about them.<br><b>Tuesdays</b><br>6:30 pm - DV8 Small Groups<br>7:00 pm - DV8<br><a class="btn btn-primary" href="https://www.facebook.com/pages/DV8-Palm-Canyon/296512720440396"><i class="fa fa-facebook-square"></i> DV8 On Facebook</a></p></div>
-        <div class="col-xs-12 col-sm-4"><h4>DV8 Sunday Morning (6th - 8th Grade)</h4>DV8 Sunday Morning is for those who want to learn and grow in their Christian lives! Every week is a little bit different, but basically it includes a time of singing and worship, great Bible study, powerful prayer times, caring friends and loads of fun. As students grow in their understanding of God's Word, they will also get to know God in a personal way and see how their lives can be changed by knowing Him.<br><b>Sundays</b><br>9:00 am</div>
-        <div class="col-xs-12 col-sm-4"><h4>Reality (February-June)</h4>Reality is for 8th grade students only. Our goal is to help 8th graders become real with God and real with each other (obeying the two greatest commandments given to us by Jesus in Matthew 11: 34-40). This is a committed group and an application is required.</div></div></div>
+        <div class="col-xs-12 col-sm-6"><h4>DV8 Sunday Morning (6th - 8th Grade)</h4>DV8 Sunday Morning is for those who want to learn and grow in their Christian lives! Every week is a little bit different, but basically it includes a time of singing and worship, great Bible study, powerful prayer times, caring friends and loads of fun. As students grow in their understanding of God's Word, they will also get to know God in a personal way and see how their lives can be changed by knowing Him.<br><b>Sundays</b><br>9:00 am</div>
+        </div>
         <hr>
         <div class="row">
         <h2 class="text-center">HIGH SCHOOL</h2>
         <div class="col-xs-12 col-sm-4"><h4>ImpACT (9th - 12th Grade)</h4>
-        ImpACT is the place for high school students to come together and learn what it means to live a life that is an impact on the people around us! When we get together it is to make a difference in our lives and in our world (usually in incredibly fun and creative ways).<br><b>Wednesdays</b><br>7:00 pm - 8:45 pm<br><a class="btn btn-primary" href="https://www.facebook.com/impact.palmcanyon?fref=ts"><i class="fa fa-facebook-square"></i> ImpACT on Facebook</a></div>
+        ImpACT is the place for high school students to come together and learn what it means to live a life that is an impact on the people around us! When we get together it is to make a difference in our lives and in our world (usually in incredibly fun and creative ways).<br><b>Wednesdays</b><br>7:00 pm - 8:45 pm<br><a class="btn btn-primary" href="https://www.facebook.com/impactpalmcanyonchurch"><i class="fa fa-facebook-square"></i> ImpACT on Facebook</a></div>
         <div class="col-xs-12 col-sm-4"><h4>Impact Sunday Morning (9th - 12th Grade)</h4>
         Impact Sunday Morning is for those who want to raise their level of commitment to God. During this time we take a serious look at God's Word and what it means to us today.<br><b>Sundays</b><br>9:00 am</div>
-        <div class="col-xs-12 col-sm-4"><h4>CORE</h4>CORE focuses on four areas: COMMIT (to God first and then one another), OBEY (God, demonstrating your love to Him through your actions), REACH (out to others), and ENCOURAGE (others through your words and actions). This is a committed group and an application is required.</div></div>
+        <div class="col-xs-12 col-sm-4"><h4>CORE</h4></div></div>
         <hr>
         <div class="row">
-       
+
         <div class="col-xs-12"><h3>Special Events (6th-12th Grade)</h3>Special Events take place all the time and include so many outrageous ideas and memories, we couldn't list them all here! They do include Winter Camp, Summer Camp, Missions Trips, game nights, concerts, beach trips, overnighters, and much more!<br><br>Special events create special memories and unique experiences. They are a great way to bring people closer to each other and to God by getting away and exploring new things.</div></div>
-        <hr>
-        <div class="row">
-        
-        <h2 class="text-center">DISCIPLESHIP (Middle School & High School)</h2>
-        <div class="col-xs-12 col-sm-6"><h3>One-on-one Discipleship</h3>Discipleship is a word that describes the process used by Jesus with His disciples to teach them how to have a relationship with God. In our Student Ministries we believe in doing discipleship one-on-one so that students can get as much out of it as possible. "Discipleship I" and "Discipleship II" will help students understand what it means to be a Christian, how to know you are a Christian, and how to grow as a Christian.</div>
-        <div class="col-xs-12 col-sm-6"><b>Heart Matters</b><br>Heart Matters (Psalm 119:11) is composed of six volumes; each filled with five Bible verses for students to memorize, and also a Bible study for each verse so they know what they are memorizing. This becomes another way for students to spend time with God and learn from His Word, while setting their own schedule.</p></div>
-        </div>
-        
+
       <!-- /.row -->
       <hr>
       <div class="row" id="adult">
@@ -41,7 +34,7 @@
       <h2 class="text-center">Parent Newsletter</h2>
       Throughout the year we will send out Parent Newsletters that do one of two things. First they inform you on upcoming events that your child may have failed to mention. And second they provide you with articles that we think may be helpful to you. Below are the recent Parent Newsletters we have sent out. To view them simply click on the one you wish to view. Keep checking back for an updated Parents Newsletter.<br><br>FREE ONLINE NEWSLETTERS: There are two great resources for your family both of which are free. The first is a student newsletter for your child where they will get a weekly excerpt or article, along with fun or interesting links, "random facts", and more. The second newsletter is a weekly parent newsletter to encourage parents of teenagers and give them insights into their teenagers culture. Click the link to sign up. <a href="http://www.youthspecialties.com/freeresources/newsletters">http://www.youthspecialties.com/freeresources/newsletters </a><br><br>To get current calendars, flyers, and information with what our Student Ministries have planned click on the Downloads & Links page under "Student Ministries" and you can download and print them.
       </div>
-      
+
       </div>
 <hr class="featurette-divider">
  <? include 'features.php' ?>
